64239ed4e50df75ed5d3d1b539cf3f6a470ceec3
Хохлов Алексей <hohlovav@novotelecom.ru>
2026-05-20 11:51:39 +0700

    commands history fix

--- src/app/Models/Log.php
+++ src/app/Models/Log.php
@@ -12,27 +12,23 @@
         protected $guarded = [];
 
         public static function getLastCommands(int $uid): array {
-            $query = "
-                with pre as (
+            return array_column(DB::select(
+                "with pre as (
                     select
                        trim( e'\t\n\r\ ' from(c.name || ' ' || l.command_params)) as name,
                        l.created_at
                     from telegram.logs l
-                    left join telegram.commands c on l.command_id::varchar = c.name
-                    where l.uid = ?
+                    left join telegram.commands c on l.command_id = c.id
+                    where l.uid = $uid
                     order by l.created_at desc
                 ),
-                post as (
-                    select name, max(created_at) from pre
-                    group by 1
-                    order by 2 desc
-                )
+                 post as (
+                     select name, max(created_at) from pre
+                     group by 1
+                     order by 2 desc)
+
                 select name from post
-                limit 5
-            ";
-
-            $results = DB::connection('ssddb')->select($query, [$uid]);
-
-            return array_column($results, 'name');
+                limit 5;"
+            ), 'name');
         }
     }
